ebc31dda414200c039d8f9af71638e86a3795fcf
Михайло Тарасенко <mishabos.bos@gmail.com>
2025-12-11 14:29:12 +0200

    update

---
+++ js/task-4.js
@@ -0,0 +1,16 @@
+const formElem = document.querySelector(".login-form");
+formElem.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userData = {};
+  if (
+    formElem.elements.email.value === "" ||
+    formElem.elements.password.value === ""
+  ) {
+    alert("All form fields must be filled in");
+  } else {
+    userData.email = formElem.elements.email.value.trim();
+    userData.password = formElem.elements.password.value.trim();
+    console.log(userData);
+    formElem.reset();
+  }
+});
